7e9733475e318c76c9ffdf640eea7d53799ae06a
Stephanie Lin <slin.animation@gmail.com>
2025-11-21 17:24:59 -0500

    adds usesAvailableLetters and helper function countLetters(). All tests pass wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -8,7 +8,7 @@ export const drawLetters = () => {
     let tile = drawTile(drawPool);
 
     hand.push(tile);
-  };
+  }
   return hand;
 };
 
@@ -21,11 +21,14 @@ const createDrawPool = () => {
 
   const drawPool = [];
 
+  // create draw pool by iterating through each letter
   for (const [letter, quantity] of Object.entries(drawTable)) {
+
+    // add the letter to the draw pool by quantity
     for(let i = 0; i < quantity; i++) {
       drawPool.push(letter);
-    };
-  };
+    }
+  }
   return drawPool;
 };
 
@@ -39,16 +42,51 @@ const drawTile = drawPool => {
   drawPool[drawPool.length-1] = selectedTile;
 
   return drawPool.pop();
-}
+};
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  // count # of each letter in hand
+  const lettersInHandCounted = countLetters(lettersInHand);
+
+  // check if each letter in input is in hand
+  for (let letter of input) {
+
+    // if letter is in hand remove 1 from the hand to use
+    if ((letter in lettersInHandCounted)) {
+      lettersInHandCounted[letter] -= 1;
+
+      // if there are 0 left in hand, remove letter from hand
+      if (lettersInHandCounted[letter] == 0) {
+        delete lettersInHandCounted[letter];
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const countLetters = (lettersInHand) => {
+  let countObj = {};
+  for (const letter of lettersInHand) {
+    // add letter to counter if it isn't in there, otherwise increment
+    if (!(letter in countObj)) {
+      countObj[letter] = 1;
+    } else {
+      countObj[letter] += 1;
+    }
+  }
+  return countObj;
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
+
+const lettersInHand = ['D', 'O', 'G', 'L', 'X', 'X', 'X', 'X', 'X', 'X'];
+usesAvailableLetters('LOOL', lettersInHand);
